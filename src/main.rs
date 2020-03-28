e65a2e44707be9376df9ee138b6568eecda29648
spacemeowx2 <spacemeowx2@gmail.com>
2020-03-28 12:19:57 +0800

    disable ws

--- src/main.rs
+++ src/main.rs
@@ -41,22 +41,25 @@ async fn main() -> std::io::Result<()> {
     log::info!("Listening on {}", bind_address);
 
     let graphql_filter = juniper_warp::make_graphql_filter(schema(), make_state(&udp_server));
-    let graphql_ws_filter = make_graphql_ws_filter(schema(), make_state(&udp_server));
+    // let graphql_ws_filter = make_graphql_ws_filter(schema(), make_state(&udp_server));
 
     let socket_addr: &SocketAddr = &bind_address.parse().unwrap();
 
     let log = warp::log("warp_server");
-    let routes = (warp::get()
-        .and(graphql_ws_filter))
-    .or(warp::path("info")
-        .and(make_state(&udp_server))
-        .and_then(server_info)
+    let routes = (
+        warp::path("info")
+            .and(make_state(&udp_server))
+            .and_then(server_info)
+        .or(warp::post()
+            .and(graphql_filter))
+        // TODO: enable ws until https://github.com/graphql-rust/juniper/issues/589 is fixed
+        // .or(
+        //     warp::get()
+        //     .and(graphql_ws_filter))
     )
     .or(warp::get()
         .and(juniper_warp::playground_filter("/", Some("/"))))
-    .or(warp::post()
-        .and(graphql_filter))
-    .with(log);
+        .with(log);
 
     warp::serve(routes)
         .run(*socket_addr)
