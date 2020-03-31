a7a163e1ed89b2584c6efbfe73c20508909df1d3
spacemeowx2 <spacemeowx2@gmail.com>
2020-03-31 17:43:23 +0800

    fix #2

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
 use std::process::Command;
 fn main() {
-    // note: add error checking yourself.
-    let output = Command::new("git").args(&["rev-parse", "HEAD"]).output().unwrap();
-    let git_hash = String::from_utf8(output.stdout).unwrap();
+    let output = Command::new("git").args(&["rev-parse", "HEAD"]).output().unwrap_or("UNKNOWN");
+    let git_hash = String::from_utf8(output.stdout).expect("Failed to parse string");
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
 }
