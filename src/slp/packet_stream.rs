52f42127b5ae8879fba542d678502092aa564446
spacemeowx2 <spacemeowx2@gmail.com>
2020-05-06 21:05:41 +0800

    feat: improve performance
    before
    
    running 5 tests
    test broadcast_1000_10 ... bench:  34,606,259 ns/iter (+/- 5,255,919)
    test broadcast_1000_20 ... bench:  58,998,443 ns/iter (+/- 13,234,575)
    test broadcast_1000_50 ... bench: 140,360,450 ns/iter (+/- 16,246,247)
    test relay_1000        ... bench:  29,168,003 ns/iter (+/- 6,527,151)
    test relay_2000        ... bench:  58,947,987 ns/iter (+/- 12,623,010)
    
    after
    
    running 5 tests
    test broadcast_1000_10 ... bench:  28,133,831 ns/iter (+/- 6,445,664)
    test broadcast_1000_20 ... bench:  52,683,181 ns/iter (+/- 6,140,983)
    test broadcast_1000_50 ... bench: 131,331,972 ns/iter (+/- 18,237,695)
    test relay_1000        ... bench:  29,199,955 ns/iter (+/- 7,050,635)
    test relay_2000        ... bench:  55,990,146 ns/iter (+/- 15,453,062)

--- src/slp/packet_stream.rs
+++ src/slp/packet_stream.rs
@@ -1,37 +1,40 @@
 use super::{ACTION_TIMEOUT, InPacket, Packet, SocketAddr};
 use tokio::{net::UdpSocket, sync::mpsc};
-use tokio::select;
 
 pub type SendTo = (Packet, Vec<SocketAddr>);
 pub type PacketSender = mpsc::Sender<SendTo>;
 pub type PacketReceiver = mpsc::Receiver<InPacket>;
 pub type SendError = mpsc::error::SendError<SendTo>;
 
-pub fn packet_stream(mut socket: UdpSocket) -> (PacketSender, PacketReceiver) {
-    // let (mut rx, tx) = socket.split();
+pub fn packet_stream(socket: UdpSocket) -> (PacketSender, PacketReceiver) {
+    let (mut rx, mut tx) = socket.split();
     let (mut in_packet_tx, in_packet_rx) = mpsc::channel::<InPacket>(10);
     let (out_packet_tx, mut out_packet_rx) = mpsc::channel::<SendTo>(10);
+
+    tokio::spawn(async move {
+        while let Some((packet, addrs)) = out_packet_rx.recv().await {
+            for addr in addrs {
+                // TODO: is it safe to ignore?
+                let _ = tx.send_to(&packet, &addr).await;
+            }
+        }
+        log::info!("Send task down");
+    });
     tokio::spawn(async move {
         loop {
             let mut buf = vec![0u8; 65536];
-            select! {
-                Ok((size, addr)) = socket.recv_from(&mut buf) => {
-                    buf.truncate(size);
-                    let in_packet = InPacket::new(addr, buf);
+            if let Ok((size, addr)) = rx.recv_from(&mut buf).await {
+                buf.truncate(size);
+                let in_packet = InPacket::new(addr, buf);
 
-                    // TODO: ignore packet drop now. maybe a counter in the future
-                    let _ = in_packet_tx.send_timeout(in_packet, ACTION_TIMEOUT).await;
-                },
-                Some((packet, addrs)) = out_packet_rx.recv() => {
-                    for addr in addrs {
-                        // TODO: is it safe to ignore?
-                        let _ = socket.send_to(&packet, &addr).await;
-                    }
-                },
-                else => break,
+                // TODO: ignore packet drop now. maybe a counter in the future
+                let _ = in_packet_tx.send_timeout(in_packet, ACTION_TIMEOUT).await;
+            } else {
+                break
             }
         }
-        log::error!("Recv task down");
+        log::info!("Recv task down");
     });
+
     return (out_packet_tx, in_packet_rx)
 }
